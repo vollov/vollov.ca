75b68df6309c9945d2e5a35dd48b2839fc11d317
dustin zhang <dike.zhang@gmail.com>
2015-08-05 10:00:57 -0400

    update wsgi settings

--- django/vollov/vollov/wsgi.py
+++ django/vollov/vollov/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for vollov project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "volov.settings_prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vollov.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
